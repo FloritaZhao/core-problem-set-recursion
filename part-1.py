ed8f6cd70fa903e9b4f30110e80c9532ff721bca
Flora <141187313+FloritaZhao@users.noreply.github.com>
2025-05-01 22:02:40 -0700

    Update part-1.py

--- part-1.py
+++ part-1.py
@@ -4,14 +4,30 @@
 # the appropriate comment.
 
 # factorial
-
+def factorial(n):
+    if n < 0 or not isinstance(n,int):
+        raise ValueError("n must be a non-negative number")
+    if n < 2:
+        return 1
+    return n * factorial(n-1)
 
 
 # reverse
+def reverse(text):
+    # first word always put in the last
+    
+    if len(text) <= 1:
+        return text
+    return reverse(text[1:]) + text[0]
 
 
 
 # bunny
+
+def bunny(count):
+    if count ==0:
+        return 0
+    return 2+bunny(count-1)
 
 
 
